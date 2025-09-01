96bbbf705fe3461c094d4b7d199d791ec5290063
Charles Swartz <charles.w.swartz.vi@gmail.com>
2025-09-01 08:13:42 -0400

    Parametrize testing session with Python versions

--- noxfile.py
+++ noxfile.py
@@ -19,13 +19,22 @@ def lint(session: nox.Session) -> None:
     session.run("pre-commit", "run", "--all-files", *session.posargs)
 
 
+@nox.session(python=["3.10", "3.11", "3.12", "3.13"])
 def test(session: nox.Session) -> None:
     """Run testing suite in an isolated environment."""
-    pyproject = nox.project.load_toml("pyproject.toml")
-
-    # FIXME: For some reason `session.install(".", "--all-extras")` does not work
-    extras = ",".join(pyproject["project"]["optional-dependencies"].keys())
-    session.install(f".[{extras}]", "--reinstall-package", "hamilton-composer", silent=False)
-
-    session.install(*nox.project.dependency_groups(pyproject, "test"))
-    session.run("pytest", "--cov", *session.posargs)
+    session.run_install(
+        "uv",
+        "sync",
+        "--dev",
+        "--all-extras",
+        f"--python={session.virtualenv.location}",
+        env={"UV_PROJECT_ENVIRONMENT": session.virtualenv.location},
+    )
+    session.run(
+        "pytest",
+        "--cov=waypoint",
+        "--cov-branch",
+        "--cov-report=html",
+        "--cov-report=term",
+        *session.posargs,
+    )
