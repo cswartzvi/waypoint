4d6b99088ef75ca69802a0d95825f3c319eaafcb
Charles Swartz <charles.w.swartz.vi@gmail.com>
2025-09-01 08:12:42 -0400

    Add linting session

--- noxfile.py
+++ noxfile.py
@@ -8,10 +8,17 @@
 import nox  # pyright: ignore
 
 nox.options.default_venv_backend = "uv"
+nox.options.sessions = ["lint", "test"]
 # nox.options.envdir = ".cache"
 
 
-@nox.session()
+@nox.session(python="3.12")
+def lint(session: nox.Session) -> None:
+    """Run linters in an isolated environment."""
+    session.install("pre-commit", "pre-commit-uv")
+    session.run("pre-commit", "run", "--all-files", *session.posargs)
+
+
 def test(session: nox.Session) -> None:
     """Run testing suite in an isolated environment."""
     pyproject = nox.project.load_toml("pyproject.toml")
